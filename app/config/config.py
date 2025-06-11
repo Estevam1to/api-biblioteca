49ff46bfe5e86869b3de97469e6f6595cf414b23
Fernando Lima <eufernandoprogramador@gmail.com>
2025-06-11 16:27:57 -0300

    feat: update database configuration to use PostgreSQL and add migration scripts for database schema

--- app/config/config.py
+++ app/config/config.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     """Configurações da aplicação"""
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
-    DATABASE_URL: str = "sqlite:///database/biblioteca.db"
+    DATABASE_URL: str = "postgresql://postgres:postgres123@localhost:5432/biblioteca_db"
     LOG_LEVEL: str = "INFO"
     LOG_FILE: str = "logs/app.log"
 
