320605c6b4f366b12e135e2783dc1aee7013f8af
Luís Estevam <luisestevam33@gmail.com>
2025-06-11 13:53:33 -0300

    feat: update settings configuration to use SettingsConfigDict

--- app/config/config.py
+++ app/config/config.py
@@ -1,13 +1,13 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    """Configurações da aplicação"""
+
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
     database_url: str = "sqlite:///database/biblioteca.db"
     log_level: str = "INFO"
     log_file: str = "logs/api.log"
-
-    class Config:
-        env_file = ".env"
 
 
 settings = Settings()
